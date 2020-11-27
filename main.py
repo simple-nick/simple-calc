6c9b127f8dfd05751c3351d81d8f734203aff893
D.S <ds@mail.com>
2020-11-27 10:42:40 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def add(num_1, num_2):
+    output = num_1 + num_2
+    return output
+
+
+def subtract(num_1, num_2):
+    output = num_1 - num_2
+    return output
+
+
+def multiply():
+    pass
+
+
+def divide():
+    pass
